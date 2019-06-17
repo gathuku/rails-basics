6517c7c0140987af0fba65743ae6dba8ea23dc85
Moses Gathuku <mosesgathuku95@gmail.com>
2019-06-18 00:35:44 +0300

    validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,25 @@
 class ArticlesController < ApplicationController
   def index
     @articles=Article.all
-    
+
   end
   def show
     @article=Article.find(params[:id])
   end
+
   def new
+    @article=Article.new
   end
 
 
   def create
   @article=Article.new(active_params)
-  @article.save
-  redirect_to @article
+
+      if @article.save
+      redirect_to @article
+     else
+       render 'new'
+     end
   end
 
   private
